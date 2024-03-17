bad075a3958a75ac0c3097bca5ceb96d970a68bc
alihanayaz <alihanayaz0@gmail.com>
2024-03-17 14:04:17 +0300

    feat: migrate project to next.js

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/card.scss";
-import { useTheme } from "@/context/ThemeContext";
-
-export function Card({ children }) {
-  const { isDarkMode } = useTheme();
-  return (
-    <div className={`card ${isDarkMode ? "dark" : "light"}`}>{children}</div>
-  );
-}
